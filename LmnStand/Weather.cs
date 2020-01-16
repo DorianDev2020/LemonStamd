ba22a93a5903cd4f5856144d46e8b55ba0c10ff1
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-01-16 15:15:10 -0600

    added some more weather conditions

--- LmnStand/Weather.cs
+++ LmnStand/Weather.cs
@@ -8,7 +8,7 @@ namespace LmnStand
 {
     public class Weather
     {
-<<<<<<< HEAD:LMNStand/Weather.cs
+
         public int temperature;
         private List<string> weatherconditions;
         public string predictedforecast;
@@ -16,15 +16,14 @@ namespace LmnStand
         public Weather()
         {
             weatherconditions = new List<string>();
-            weatherconditions.Add(" Rain");
-            weatherconditions.Add(" Cloudy");
-            weatherconditions.Add(" Sunny");
-            weatherconditions.Add(" Snow");
-            weatherconditions.Add(" Overcast");
+            weatherconditions.Add("Rain");
+            weatherconditions.Add("Cloudy");
+            weatherconditions.Add("Windy");
+            weatherconditions.Add("Overcast");
+            weatherconditions.Add("Sunny");
         }
        
-=======
->>>>>>> 85c781d43dbc54d961e6d133a81792d0109c8ff4:LmnStand/Weather.cs
+
     }
     
 }
